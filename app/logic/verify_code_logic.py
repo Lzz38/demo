a0fb3fdc112e6a78649b74dc498cd446efe414f6
lzz <liuyuanyuan@yimo8.com>
2021-03-31 20:05:34 +0800

    fix content

--- app/logic/verify_code_logic.py
+++ app/logic/verify_code_logic.py
@@ -12,7 +12,7 @@ def verify_code(otp, mobile):
         if has_sent:
             raise exceptions.InvalidParamError(ErrorCode.ERROR_OTP_HAS_SENT)
         auth_code = random.randint(100000, 999999)
-        content = '[xxx],您的验证码为{},此验证码五分钟内有效'.format(auth_code, + ', 退订回T')
+        content = '[xxx],您的验证码为{},此验证码五分钟内有效, 退订回T'.format(auth_code)
         from app.tasks import send_sms_task
         send_sms_task(content, mobile)
         auth_code_cache.save_auth_code(mobile,auth_code)
